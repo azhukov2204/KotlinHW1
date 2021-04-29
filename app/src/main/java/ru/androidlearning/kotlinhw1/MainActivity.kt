c84fdbc52e2be27286c01db834089c3059fbe0c8
azhukov2204 <andrew284@mail.ri>
2021-04-30 01:18:25 +0500

    added a button and assigned a clickListener to it in the Activity

--- app/src/main/java/ru/androidlearning/kotlinhw1/MainActivity.kt
+++ app/src/main/java/ru/androidlearning/kotlinhw1/MainActivity.kt
@@ -2,11 +2,25 @@ package ru.androidlearning.kotlinhw1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import ru.androidlearning.kotlinhw1.R
+import ru.androidlearning.kotlinhw1.R.string.textViewNewText
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Добавить кнопку в разметку и повесить на неё clickListener в Activity.
+        val textView = findViewById<TextView>(R.id.textView)
+        val button1 = findViewById<Button>(R.id.button1)
+        button1.setOnClickListener {
+            textView.text = resources.getString(textViewNewText)
+        }
+
     }
 }
