dc58f65117dfa1d40762d39471e80900c440b7a7
azhukov2204 <andrew284@mail.ri>
2021-04-30 00:52:31 +0500

    MainActivity translated into Kotlin

--- app/src/main/java/ru/androidlearning/kotlinhw1/MainActivity.kt
+++ app/src/main/java/ru/androidlearning/kotlinhw1/MainActivity.kt
@@ -1,14 +1,12 @@
-package ru.androidlearning.kotlinhw1;
+package ru.androidlearning.kotlinhw1
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.androidlearning.kotlinhw1.R
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
